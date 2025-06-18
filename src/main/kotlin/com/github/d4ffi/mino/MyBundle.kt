fc279680c2e0d94e1002ca19deb37dc3ea8e446e
GitHub Action <action@github.com>
2025-06-18 05:04:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/d4ffi/mino/MyBundle.kt
+++ src/main/kotlin/com/github/d4ffi/mino/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.d4ffi.mino
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/d4ffi/mino/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/d4ffi/mino/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.d4ffi.mino.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
